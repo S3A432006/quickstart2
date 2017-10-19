fcbf9397d5fbfd8b273f340d6d2b689e9fad694c
Jia Yi Lai <anniuu7134@gmail.com>
2017-10-19 19:13:55 +0800

    建置一些對應至此控制器的路由

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,7 @@
 
 Route::get('/', function () {
     return view('welcome');
+    Route::get('/tasks', 'TaskController@index');
+    Route::post('/task', 'TaskController@store');
+    Route::delete('/task/{task}', 'TaskController@destroy');
 });
