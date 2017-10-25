f6610217eb0e1c4426a0e8cc4fef84317bbc82d4
Jia Yi Lai <anniuu7134@gmail.com>
2017-10-25 12:54:01 +0800

    注入資料庫

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,10 +6,13 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    protected $tasks;
+
     /**
-     * 建立一個新的控制器實例。
+     * 建立新的任務。
      *
-     * @return void
+     * @param  Request  $request
+     * @return Response
      */
     public function __construct()
     {
@@ -40,3 +43,19 @@ class TaskController extends Controller
         ]);
     }
 }
+/**
+ * Display a list of all of the user's task.
+ *
+ * @param  Request  $request
+ * @return Response
+ */
+public function index(Request $request)
+{
+    $tasks = Task::where('user_id', $request->user()->id)->get();
+
+    return view('tasks.index', [
+        'tasks' => $tasks,
+    ]);
+}
+
+
