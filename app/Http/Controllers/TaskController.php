297647afc42ccf8811dfc4fbb79e4e0da4faadf0
Jia Yi Lai <anniuu7134@gmail.com>
2017-10-25 12:55:07 +0800

    顯示已有的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,35 +1,23 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class TaskController extends Controller
 {
-    protected $tasks;
-
     /**
      * 建立新的任務。
      *
      * @param  Request  $request
      * @return Response
      */
-    public function __construct()
+    public function store(Request $request)
     {
         $this->validate($request, [
             'name' => 'required|max:255',
         ]);
-
-
-        $request->user()->tasks()->create([
-            'name' => $request->name,
-        ]);
-
-        return redirect('/tasks');
+        // Create The Task...
     }
-
     /**
-     * 顯示使用者所有任務的清單。
+     * Display a list of all of the user's task.
      *
      * @param  Request  $request
      * @return Response
@@ -42,20 +30,5 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
 }
-/**
- * Display a list of all of the user's task.
- *
- * @param  Request  $request
- * @return Response
- */
-public function index(Request $request)
-{
-    $tasks = Task::where('user_id', $request->user()->id)->get();
-
-    return view('tasks.index', [
-        'tasks' => $tasks,
-    ]);
-}
-
-
